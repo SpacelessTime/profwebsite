978d28e72c08204a717ec8bfb2db319ae2876d80
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-12-01 16:57:57 -0500

    initialize

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='navbar'>
+        <h1 className='Title'> Julian Petrisky</h1>
+
+      </nav>
+    <div className='leftContent'>
+      <div className='leftTop'> left top content  </div>           
+      <div className='leftMid'>  left middle content </div>
+      <div className='leftBottom'> left bottom content    </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+<footer className='footer'></footer>
+    </div>
+  );
+}
+
+export default App;
