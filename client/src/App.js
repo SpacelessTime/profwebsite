29f89052b2febb5277c9f8a67ad99c0f2cbbc6a6
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-12-16 14:39:49 -0500

    upgraded grid

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <nav className='navbar'>
         <h1 className='Title'> Julian Petrisky</h1>
+        
+</nav>
+    <div className='contentGrid'>
+      <div className='leftTop'> left top content  </div>   
+      <div className='midTop'>   middle top content  </div> 
+      <div className='rightTop'>  right top content </div>   
 
-      </nav>
-    <div className='leftContent'>
-      <div className='leftTop'> left top content  </div>           
       <div className='leftMid'>  left middle content </div>
-      <div className='leftBottom'> left bottom content    </div>
-    </div>
+      <div className='middle'>dead middle content</div>
+      <div className='rightMid'> right middle content</div>
+
+      <div className='leftBottom'> left bottom content </div>
+      <div className='midBottom'> middle bottom content</div>
+      <div className='rightBottom'> right bottom content</div>
+</div>
+      
+
+
 
 
 
