2af30a30ac93a4049c9332b9fefda291977d5656
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-12-17 20:14:14 -0500

    grid kinks worked out

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,69 @@
 
 import './App.css';
+import {Route, Link, Switch } from 'react-router-dom';
+
+
+
 
 function App() {
   return (
     <div className='App'>
       <nav className='navbar'>
         <h1 className='Title'> Julian Petrisky</h1>
-        
-</nav>
+        <Link to="/about">About</Link>
+  </nav>
     <div className='contentGrid'>
-      <div className='leftTop'> left top content  </div>   
-      <div className='midTop'>   middle top content  </div> 
-      <div className='rightTop'>  right top content </div>   
+      <div className='leftTop'> left top content 
 
-      <div className='leftMid'>  left middle content </div>
-      <div className='middle'>dead middle content</div>
-      <div className='rightMid'> right middle content</div>
+          <img className='Image'src={'./assets/MTOL0228.JPG'} alt='sunset'/>
 
-      <div className='leftBottom'> left bottom content </div>
-      <div className='midBottom'> middle bottom content</div>
-      <div className='rightBottom'> right bottom content</div>
+       </div>   
+      <div className='midTop'>   middle top content  
+
+          <img className='Image'src={'./assets/JLIZ1197.JPG'} alt='sunset'/>
+
+      </div> 
+      <div className='rightTop'>  right top content
+
+          <img className='Image'src={'./assets/QNOC8648.JPG'} alt='sunset'/>
+
+       </div>   
+
+
+      <div className='leftMid'>  left middle content 
+
+        <img className='Image' src={'./assets/RFSL3209.JPG'} alt=''/>
+
+      </div>
+
+      <div className='middle'>dead middle content
+
+        <img className='Image' src={'./assets/SDCQ6636.JPG'} alt=''/>
+
+      </div>
+
+      <div className='rightMid'> right middle content
+
+        <img className='Image' src={'./assets/VSIV2438.JPG'} alt=''/>
+      </div>
+
+      <div className='leftBottom'> left bottom content 
+
+        <img className='Image' src={'./assets/NJQV8408.JPG'} alt=''/>
+
+      </div>
+
+      <div className='midBottom'> middle bottom content
+
+        <img className='Image' src={'./assets/WHPM3278.JPG'} alt=''/>
+
+
+      </div>
+      <div className='rightBottom'> right bottom content
+
+        <img className='Image' src={'./assets/XDDM5070.JPG'} alt=''/>
+
+      </div>
 </div>
       
 
